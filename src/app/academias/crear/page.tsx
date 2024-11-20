e13e69e18f663526ac61ed97c8e72a7ffef36e92
Nico <medinanico93@gmail.com>
2024-11-20 15:34:16 -0300

    agregado de diseño

--- src/app/academias/crear/page.tsx
+++ src/app/academias/crear/page.tsx
@@ -33,38 +33,97 @@ function CrearAcademia() {
   };
 
   return (
-    <div className="justify-center h-[calc(100vh-4rem)] flex items-center">
-      <form onSubmit={handleSubmit} className="bg-neutral-950 px-8 py-10 w-3/12">
+    <div className="w-[390px] flex flex-col items-center gap-5">
+      <p className="text-xl font-bold justify-self-center">Crear Academia</p>
+      <form onSubmit={handleSubmit} className="w-[80%]">
         {error && <div className="bg-red-500 text-white p-2 mb-2">{error}</div>}
-        <h1 className="text-4xl font-bold mb-7">Crear Academia</h1>
 
-        <label className="text-slate-300">Nombre de la Academia:</label>
-        <input type="text" name="nombre_academia" placeholder="Nombre de la Academia" className="bg-zinc-800 px-4 py-2 block mb-2 w-full" required />
+        
+        <div className="relative mb-6">
+          <input
+            type="text"
+            name="nombre_academia"
+            className="form-input peer"
+            placeholder=" "
+            required
+          />
+          <label className="form-label">Nombre</label>
+        </div>
 
-        <label className="text-slate-300">País:</label>
-        <input type="text" name="pais" placeholder="País" className="bg-zinc-800 px-4 py-2 block mb-2 w-full" required />
+        
+        <div className="relative mb-6">
+          <input
+            type="text"
+            name="pais"
+            className="form-input peer"
+            placeholder=" "
+            required
+          />
+          <label className="form-label">País</label>
+        </div>
 
-        <label className="text-slate-300">Provincia:</label>
-        <input type="text" name="provincia" placeholder="Provincia" className="bg-zinc-800 px-4 py-2 block mb-2 w-full" required />
+        
+        <div className="relative mb-6">
+          <input
+            type="text"
+            name="provincia"
+            className="form-input peer"
+            placeholder=" "
+            required
+          />
+          <label className="form-label">Provincia</label>
+        </div>
 
-        <label className="text-slate-300">Localidad:</label>
-        <input type="text" name="localidad" placeholder="Localidad" className="bg-zinc-800 px-4 py-2 block mb-2 w-full" required />
+        
+        <div className="relative mb-6">
+          <input
+            type="text"
+            name="localidad"
+            className="form-input peer"
+            placeholder=" "
+            required
+          />
+          <label className="form-label">Localidad</label>
+        </div>
 
-        <label className="text-slate-300">Descripción:</label>
-        <textarea name="descripcion" placeholder="Descripción" className="bg-zinc-800 px-4 py-2 block mb-2 w-full" />
+       
+        <div className="relative mb-6">
+          <textarea
+            name="descripcion"
+            className="form-input peer"
+            placeholder=" "
+          ></textarea>
+          <label className="form-label">Descripción</label>
+        </div>
 
-        <label className="text-slate-300">Tipo de Disciplina:</label>
-        <select name="tipo_disciplina" className="bg-zinc-800 px-4 py-2 block mb-2 w-full" required>
-          <option value="running">Running</option>
-          <option value="trekking">Trekking</option>
-          <option value="otros">Otros</option>
-        </select>
+       
+        <div className="relative mb-6">
+          <select name="tipo_disciplina" className="form-input peer" placeholder=" " required>
+            <option value="running">Running</option>
+            <option value="trekking">Trekking</option>
+            <option value="otros">Ciclismo</option>
+          </select>
+          <label className="form-label">Tipo de disciplina</label>
+        </div>
 
-        <label className="text-slate-300">Teléfono:</label>
-        <input type="text" name="telefono" placeholder="Teléfono" className="bg-zinc-800 px-4 py-2 block mb-2 w-full" />
+       
+        <div className="relative mb-6">
+          <input
+            type="text"
+            name="telefono"
+            className="form-input peer"
+            placeholder=" "
+          />
+          <label className="form-label">Teléfono</label>
+        </div>
 
-        <button className="bg-blue-500 text-white px-4 py-2 block w-full mt-4">
+        
+        <button className="bg-[#FF9A3D] text-[#333] font-bold px-4 py-2 block w-full mt-4 rounded-[10px]">
           Crear Academia
+        </button>
+
+        <button className="bg-[#f4f4f4] border-2 border-[#FF9A3D] text-[#FF9A3D] font-bold px-4 py-2 block w-full mt-4 rounded-[10px]">
+          Atrás
         </button>
       </form>
     </div>
