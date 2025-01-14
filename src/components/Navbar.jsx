456d6d46e97b3530dfa1ae82b6f54e20a4b6c6ee
Nico <medinanico93@gmail.com>
2025-01-14 10:18:13 -0300

    cambios en dasboard y mas

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,10 +9,9 @@ function Navbar() {
   // Mostrar un loader mientras se verifica la sesión
   if (status === "loading") return null;
 
-
   if (!session) return null;
 
-  const userRole = session.user?.role; 
+  const userRole = session.user?.role;
 
   return (
     <nav
@@ -20,7 +19,12 @@ function Navbar() {
       style={{ zIndex: 50 }}
     >
       <div className="flex w-full items-center">
-        <Link className="w-[33%] flex justify-center" href="/dashboard">
+
+        <ul className="flex w-full justify-around items-center">
+          {userRole === "dueño de academia" ? (
+            <>
+            <li>
+            <Link className="" href="/dashboard">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             height="35px"
@@ -32,10 +36,8 @@ function Navbar() {
             <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
           </svg>
         </Link>
+            </li>
 
-        <ul className="flex w-[66%] justify-around items-center">
-          {userRole === "dueño de academia" ? (
-            <>
               <li>
                 <Link href="/academias">
                   <svg
@@ -50,6 +52,21 @@ function Navbar() {
                     <g>
                       <path d="M12,12.75c1.63,0,3.07,0.39,4.24,0.9c1.08,0.48,1.76,1.56,1.76,2.73L18,18H6l0-1.61c0-1.18,0.68-2.26,1.76-2.73 C8.93,13.14,10.37,12.75,12,12.75z M4,13c1.1,0,2-0.9,2-2c0-1.1-0.9-2-2-2s-2,0.9-2,2C2,12.1,2.9,13,4,13z M5.13,14.1 C4.76,14.04,4.39,14,4,14c-0.99,0-1.93,0.21-2.78,0.58C0.48,14.9,0,15.62,0,16.43V18l4.5,0v-1.61C4.5,15.56,4.73,14.78,5.13,14.1z M20,13c1.1,0,2-0.9,2-2c0-1.1-0.9-2-2-2s-2,0.9-2,2C18,12.1,18.9,13,20,13z M24,16.43c0-0.81-0.48-1.53-1.22-1.85 C21.93,14.21,20.99,14,20,14c-0.39,0-0.76,0.04-1.13,0.1c0.4,0.68,0.63,1.46,0.63,2.29V18l4.5,0V16.43z M12,6c1.66,0,3,1.34,3,3 c0,1.66-1.34,3-3,3s-3-1.34-3-3C9,7.34,10.34,6,12,6z" />
                     </g>
+                  </svg>
+                </Link>
+              </li>
+
+              <li>
+                <Link href="/academias/crear">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="30px"
+                    viewBox="0 0 24 24"
+                    width="30px"
+                    fill="#333"
+                  >
+                    <path d="M0 0h24v24H0z" fill="none" />
+                    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm5 11h-4v4h-2v-4H7v-2h4V7h2v4h4v2z" />
                   </svg>
                 </Link>
               </li>
@@ -71,6 +88,38 @@ function Navbar() {
             </>
           ) : (
             <>
+              <li>
+            <Link className="" href="/dashboard">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="35px"
+            viewBox="0 0 24 24"
+            width="35px"
+            fill="#333"
+          >
+            <path d="M0 0h24v24H0z" fill="none" />
+            <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
+          </svg>
+        </Link>
+            </li>
+              <li>
+                <Link href="/academias">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    enable-background="new 0 0 24 24"
+                    height="35px"
+                    viewBox="0 0 24 24"
+                    width="35px"
+                    fill="#333"
+                  >
+                    <rect fill="none" height="24" width="24" />
+                    <g>
+                      <path d="M12,12.75c1.63,0,3.07,0.39,4.24,0.9c1.08,0.48,1.76,1.56,1.76,2.73L18,18H6l0-1.61c0-1.18,0.68-2.26,1.76-2.73 C8.93,13.14,10.37,12.75,12,12.75z M4,13c1.1,0,2-0.9,2-2c0-1.1-0.9-2-2-2s-2,0.9-2,2C2,12.1,2.9,13,4,13z M5.13,14.1 C4.76,14.04,4.39,14,4,14c-0.99,0-1.93,0.21-2.78,0.58C0.48,14.9,0,15.62,0,16.43V18l4.5,0v-1.61C4.5,15.56,4.73,14.78,5.13,14.1z M20,13c1.1,0,2-0.9,2-2c0-1.1-0.9-2-2-2s-2,0.9-2,2C18,12.1,18.9,13,20,13z M24,16.43c0-0.81-0.48-1.53-1.22-1.85 C21.93,14.21,20.99,14,20,14c-0.39,0-0.76,0.04-1.13,0.1c0.4,0.68,0.63,1.46,0.63,2.29V18l4.5,0V16.43z M12,6c1.66,0,3,1.34,3,3 c0,1.66-1.34,3-3,3s-3-1.34-3-3C9,7.34,10.34,6,12,6z" />
+                    </g>
+                  </svg>
+                </Link>
+              </li>
+
               <li>
                 <Link href="/dashboard/profile">
                   <svg
