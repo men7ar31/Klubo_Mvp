b9e2162efa0a2753bc67f78c108ab202ac103c39
BautistaConta <bautistaconta0@gmail.com>
2025-02-09 20:46:01 -0300

    Cambios en la notificaciones y boton de pago

--- src/components/TopContainer.jsx
+++ src/components/TopContainer.jsx
@@ -6,6 +6,7 @@ import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { getProfileImage } from "@/app/api/profile/getProfileImage";
 import Link from "next/link";
+import axios from "axios";
 
 const TopContainer = () => {
   const { data: session, status } = useSession();
@@ -16,13 +17,25 @@ const TopContainer = () => {
     email: session?.user.email || "",
   });
   const [profileImage, setProfileImage] = useState(null);
+  const [SolicitudesPendientes, setSolicitudesPendientes] = useState(false); // estado solicitudes pendientes
 
+  // Obtener solicitudes
   useEffect(() => {
     if (session?.user) {
-      setFormData({
-        fullname: session.user.fullname || "",
-        email: session.user.email || "",
-      });
+      // Verificar las solicitudes del usuario cuando el componente se monta
+      const fetchSolicitudes = async () => {
+        try {
+          const response = await axios.get("/api/academias/solicitudes");
+          const solicitudesData = response.data;
+          // Establecer si hay solicitudes pendientes
+          setSolicitudesPendientes(solicitudesData.some((solicitud) => solicitud.estado === "pendiente"));
+        } catch (error) {
+          console.error("Error al cargar las solicitudes", error);
+        }
+      };
+
+      fetchSolicitudes();
+
 
       // Intentar obtener la imagen del perfil
       const loadProfileImage = async () => {
@@ -78,7 +91,9 @@ const TopContainer = () => {
         <p className="flex font-normal text-sm">{session?.user?.fullname || "Usuario no identificado"}</p>
       </div>
       <div
-        className="rounded-full border border-[#999999] shadow-xl h-[40px] w-[40px] flex justify-center items-center cursor-pointer"
+        className={`rounded-full border shadow-xl h-[40px] w-[40px] flex justify-center items-center cursor-pointer ${
+          SolicitudesPendientes ? "bg[#E5E5E5]" : "bg-[#B9B6B5]"
+        }`}
         onClick={handleNotificationClick}
       >
         <svg
@@ -86,7 +101,7 @@ const TopContainer = () => {
           height="25px"
           viewBox="0 0 24 24"
           width="25px"
-          fill="#999"
+          fill="#fff"
         >
           <path d="M12 22c1.1 0 2-.9 2-2h-4c0 1.1.89 2 2 2zm6-6v-5c0-3.07-1.64-5.64-4.5-6.32V4c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5 1.5v.68C7.63 5.36 6 7.92 6 11v5l-2 2v1h16v-1l-2-2z" />
         </svg>
