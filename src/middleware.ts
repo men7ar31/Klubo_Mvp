9650ac867ace93bbecd18be687b9111877516a10
Nico <medinanico93@gmail.com>
2025-02-28 15:25:42 -0300

    Cambio en el auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,3 @@
 export { default } from "next-auth/middleware";
 
-export const config = {
-  matcher: ["/", "/:path*"], // Esto protege solo la raíz y todas las demás rutas, excepto /dashboard
-};
+export const config = { matcher: ["/", "/dashboard/:path*"] };
