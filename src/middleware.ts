f8bf4c88fd5f62e65e59f52b5da84708bb3d7baf
Nico <medinanico93@gmail.com>
2025-02-28 15:37:55 -0300

    Cambio en el auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,3 @@
-import { withAuth } from "next-auth/middleware";
+export { default } from "next-auth/middleware";
 
-export default withAuth({
-  pages: {
-    signIn: "/login",
-  },
-  callbacks: {
-    authorized({ req, token }) {
-      console.log("Token en Middleware:", token); // Ver si existe un token en consola
-      return !!token; // Solo permite acceso si hay token
-    },
-  },
-});
-
-export const config = { matcher: ["/about/:path*"] };
+export const config = { matcher: ["/", "/dashboard/:path*"] };
